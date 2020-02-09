7d194aaf66c64f6b0767c2965aa0fd761ec33e69
zohaibkhan003 <45927664+zohaibkhan003@users.noreply.github.com>
2020-02-08 18:55:34 -0800

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -1,32 +1,89 @@
-
 #include <iostream>
 using namespace std;
 
-void mystrcat(char destination[], const char source[])
+class Rational
 {
-    int i;
-    int w;
-    int j;
-    int l;
-    for(i = 0; destination[i] != '\0'; i++); // gets num before null - 3
-    for(w = 0; source[w] != '\0'; w++); // - 3
-    l = w;
-    w = 0;
-    for(j = 0; j < l; j++)
-    {
-        destination[i] = source[w];
-        w++;
-        i++;
-    }
-    destination[i] = '\0';
-    
-}
+   private:
+   int numer;
+   int denom; 
+ 
+   public:
+   Rational();
+   Rational(int); 
+   Rational(int, int); 
+   const Rational add(const Rational &) const; 
+   const Rational subtract(const Rational &) const; 
+   const Rational multiply(const Rational &) const; 
+   const Rational divide(const Rational &) const; 
+   void display() const; 
+};
+   Rational::Rational() // first construct
+   {
+      numer = 0;
+      denom = 1;
+      return;
+   }
+   Rational::Rational(int a)  // second construct
+   {
+      numer = a;
+      denom = 1;
+      return;
+   }
+   Rational::Rational(int a, int b) // third construct
+   {
+      numer = a;
+      denom = b;
+      return;
+   }
+   const Rational Rational::add(const Rational &b) const
+   {
+      Rational j;
+      j.numer =  ((numer * b.denom) + (denom * b.numer));
+      j.denom = (denom * b.denom);
+      return j;
+   }
+   const Rational Rational::subtract(const Rational &b) const
+   {
+      Rational j;
+      j.numer =  ((numer * b.denom) - (denom * b.numer));
+      j.denom = (denom * b.denom);
+      return j;
+   }
+   const Rational Rational::multiply(const Rational &b) const
+   {
+      Rational j;
+      j.numer =  (numer * b.numer);
+      j.denom = (denom * b.denom);
+      return j;
+   }
+   const Rational Rational::divide(const Rational &b) const
+   {
+      Rational j;
+      j.numer =  (numer * b.denom);
+      j.denom =  (denom * b.numer);
+      return j;
+   }
+   void Rational::display() const
+   {
+      cout << numer << '/' << denom;
+      return;
+   }
+   
 int main()
 {
-   char s[80] = "";
-   char d[80] = "dog";
-   mystrcat(s,d);
-   cout << s << endl;
+   Rational a(1,4);
+   Rational b(1,4);
+   Rational c;
+   Rational d;
+   Rational e;
+   Rational f;
+   c = a.add(b);
+   d = a.subtract(b);
+   e = a.multiply(b);
+   f = a.divide(b);
+   c.display();
+   d.display();
+   e.display();
+   f.display();
    return 0;
 }
-
